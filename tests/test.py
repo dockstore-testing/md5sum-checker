c2ca065a990224fc87d5b378a26b726f5623430b
Emily Soth <esoth@ucsc.edu>
2019-06-24 16:56:43 -0700

    added arguments as list

--- tests/test.py
+++ tests/test.py
@@ -4,21 +4,21 @@ import unittest
 
 class TestMd5sumChecker(unittest.TestCase):
     def test_cwl_success(self):
-        process = subprocess.run("cwltool checker-workflow-wrapping-tool.cwl checker-input-cwl.json")
+        process = subprocess.run(["cwltool", "checker-workflow-wrapping-tool.cwl", "checker-input-cwl.json"])
         self.assertEqual(process.returncode, 0)
 
     def test_cwl_failure(self):
-        process = subprocess.run("cwltool checker-workflow-wrapping-tool.cwl checker-fail-cwl.json")
+        process = subprocess.run(["cwltool", "checker-workflow-wrapping-tool.cwl", "checker-fail-cwl.json"])
         self.assertEqual(process.returncode, 1)
 
     def test_wdl_success(self):
-        process = subprocess.run(
-            "java -jar cromwell-42.jar run checker-workflow-wrapping-workflow.wdl -i md5sum-wdl.json")
+        process = subprocess.run(["java", "-jar", "cromwell-42.jar", "run", "checker-workflow-wrapping-workflow.wdl",
+                                  "-i", "md5sum-wdl.json"])
         self.assertEqual(process.returncode, 0)
 
     def test_wdl_failure(self):
-        process = subprocess.run(
-            "java -jar cromwell-42.jar run checker-workflow-wrapping-workflow.wdl -i md5sum-fail-wdl.json")
+        process = subprocess.run(["java", "-jar", "cromwell-42.jar", "run", "checker-workflow-wrapping-workflow.wdl",
+                                  "-i", "md5sum-fail-wdl.json"])
         self.assertEqual(process.returncode, 1)
 
 
