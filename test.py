e5814aa03b45b96651ecc7a5c3939ed54159ff5f
Emily Soth <esoth@ucsc.edu>
2019-06-24 16:22:59 -0700

    trying out travis ci

---
+++ test.py
@@ -0,0 +1,23 @@
+import subprocess
+import unittest
+
+
+class TestMd5sumChecker(unittest.TestCase):
+    def test_cwl_success(self):
+        process = subprocess.run("cwltool checker-workflow-wrapping-tool.cwl checker-input-cwl.json")
+        self.assertEqual(process.returncode, 0)
+
+    def test_cwl_failure(self):
+        process = subprocess.run("cwltool checker-workflow-wrapping-tool.cwl checker-fail-cwl.json")
+        self.assertEqual(process.returncode, 1)
+
+    def test_wdl_success(self):
+        process = subprocess.run(
+            "java -jar cromwell-42.jar run checker-workflow-wrapping-workflow.wdl -i md5sum-wdl.json")
+        self.assertEqual(process.returncode, 0)
+
+    def test_wdl_failure(self):
+        process = subprocess.run(
+            "java -jar cromwell-42.jar run checker-workflow-wrapping-workflow.wdl -i md5sum-fail-wdl.json")
+        self.assertEqual(process.returncode, 1)
+
